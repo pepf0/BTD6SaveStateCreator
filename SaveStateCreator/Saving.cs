59d75c583ad5b5859171595031674264f916c31b
Pepf <pepf64yt@gmail.com>
2025-07-05 22:57:08 +0200

    Created "GetSavePath" Method in order to get the BTD6 Save Path.

--- SaveStateCreator/Saving.cs
+++ SaveStateCreator/Saving.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,7 +10,9 @@ namespace SaveStateCreator
 {
     internal static class Saving
     {
-        static void CreateSaveState(string btdSavesPath, string saveToPath)
+        public static readonly string appdataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "BTD6SaveStateCreator");
+        public static readonly string saveToPath = Path.Combine(appdataPath, "Profile.Save");
+        static void CreateSaveState(string btdSavesPath)
         {
             string oldSavesPath = Path.Combine(saveToPath, "OldSaves");
             if (!Directory.Exists(saveToPath))
@@ -20,13 +23,10 @@ namespace SaveStateCreator
 
             if (File.Exists(btdSavesPath))
             {
-                string saveFileName = Path.GetFileName(btdSavesPath);
-                string destinationPath = Path.Combine(saveToPath, saveFileName);
-
-                if (File.Exists(destinationPath))
+                if (File.Exists(saveToPath))
                 {
-                    string nameWithoutExtension = Path.GetFileNameWithoutExtension(saveFileName);
-                    string extension = Path.GetExtension(saveFileName);
+                    string nameWithoutExtension = Path.GetFileNameWithoutExtension(saveToPath);
+                    string extension = Path.GetExtension(saveToPath);
 
                     int index = 1;
                     string newOldPath;
@@ -37,13 +37,14 @@ namespace SaveStateCreator
                         index++;
                     } while (File.Exists(newOldPath));
 
-                    File.Move(destinationPath, newOldPath);
+                    File.Move(saveToPath, newOldPath);
                 }
-                File.Copy(btdSavesPath, destinationPath, true);
+                File.Copy(btdSavesPath, saveToPath, true);
             }
             else
                 throw new ArgumentException($"The file {btdSavesPath} does not exist or is not a valid save file.", nameof(btdSavesPath));
         }
+
         static void LoadSaveState(string btdSavesPath, string saveToPath)
         {
             string saveFileName = Path.GetFileName(btdSavesPath);
@@ -51,7 +52,19 @@ namespace SaveStateCreator
 
             if (!File.Exists(sourcePath))
                 throw new ArgumentException($"The path {saveToPath} does not contain a valid save file.", nameof(saveToPath));
-                File.Copy(sourcePath, btdSavesPath, true);
+            File.Copy(sourcePath, btdSavesPath, true);
+        }
+
+        static string? GetSavePath()
+        {
+            var steamPath = Registry.CurrentUser.OpenSubKey(@"Software\Valve\Steam")?.GetValue("SteamPath") as string;
+            if (steamPath == null) return null;
+
+            var userdata = Path.Combine(steamPath, "userdata");
+            var userId = Directory.GetDirectories(userdata)?[0];
+            if (userId == null) return null;
+
+            return Path.Combine(userId, "960090", "local", "link", "PRODUCTION", "current", "Profile.Save");
         }
     }
 }
